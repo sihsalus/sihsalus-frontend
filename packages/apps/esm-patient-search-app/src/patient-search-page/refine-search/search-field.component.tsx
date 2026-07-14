a579281411f30dc88f2ebb791a2e295bceea9b00
UltimateCosmic <johan.amador@pucp.edu.pe>
2026-07-14 06:21:24 -0500

    fix lint error

--- packages/apps/esm-patient-search-app/src/patient-search-page/refine-search/search-field.component.tsx
+++ packages/apps/esm-patient-search-app/src/patient-search-page/refine-search/search-field.component.tsx
@@ -93,7 +93,9 @@ const BirthdateSearchField: React.FC<Omit<SearchFieldProps, 'field'>> = ({ contr
                       minDate={oldestAllowedBirthdate ? calendarDateToLocalDate(oldestAllowedBirthdate) : undefined}
                       maxDate={calendarDateToLocalDate(referenceDate)}
                       invalid={!!error}
-                      invalidText={error?.message}
+                      invalidText={
+                        dayState.error?.message ?? monthState.error?.message ?? yearState.error?.message
+                      }
                       size={isTablet ? 'lg' : 'md'}
                     />
                   );

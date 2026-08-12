f2ebf62445a8a8d89f63ee5eceaa2b011453429d
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2026-08-11 23:02:46 -0500

    chore(clinical-ui): remove unreachable legacy code

--- packages/apps/esm-patient-notes-app/src/dashboard.meta.ts
+++
@@ -1,9 +0,0 @@
-import { type DashboardLinkConfig } from '@openmrs/esm-patient-common-lib';
-
-export const dashboardMeta: DashboardLinkConfig & { slot: string } = {
-  moduleName: '@sihsalus/esm-patient-notes-app',
-  slot: 'patient-chart-encounters-dashboard-slot',
-  title: 'Encounters',
-  path: 'Encounters',
-  icon: 'omrs-icon-calendar-heat-map',
-};

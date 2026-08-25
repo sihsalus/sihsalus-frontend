4fbe5a25c0b0cd8cedce992dca32cfdae47d1a23
Johan Amador <johan.amador@pucp.edu.pe>
2026-08-24 23:58:47 -0500

    feat(laboratory): export completed exams report

--- packages/apps/esm-laboratory-app/src/lab-tabs/data-table-extensions/completed-lab-requests-table.extension.tsx
+++ packages/apps/esm-laboratory-app/src/lab-tabs/data-table-extensions/completed-lab-requests-table.extension.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import OrdersDataTable from '../../components/orders-table/orders-data-table.component';
 
 const CompletedLabRequestsTable: React.FC = () => {
-  return <OrdersDataTable fulfillerStatus="COMPLETED" excludeCanceledAndDiscontinuedOrders={false} />;
+  return (
+    <OrdersDataTable
+      fulfillerStatus="COMPLETED"
+      excludeCanceledAndDiscontinuedOrders={false}
+      showCompletedReportDownload
+    />
+  );
 };
 
 export default CompletedLabRequestsTable;
